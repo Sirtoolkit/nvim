2be5286d3d02b751152411df85df891d9b77babb
Cesar Posada <cesarleonardo537@gmail.com>
2021-02-16 10:19:34 -0600

    auto push

--- lua/gitsigns-nvim.lua
+++ lua/gitsigns-nvim.lua
@@ -1,9 +1,9 @@
 require'gitsigns'.setup {
 signs = {
-  add          = {hl = 'GitGutterAdd'   , text = '│', numhl='GitSignsAddNr'},
-  change       = {hl = 'GitGutterChange', text = '│', numhl='GitSignsChangeNr'},
-  delete       = {hl = 'GitGutterDelete', text = '│', numhl='GitSignsDeleteNr'},
-  topdelete    = {hl = 'GitGutterDelete', text = '│', numhl='GitSignsDeleteNr'},
-  changedelete = {hl = 'GitGutterChange', text = '│', numhl='GitSignsChangeNr'},
+  add          = {hl = 'GitGutterAdd'   , text = '▎', numhl='GitSignsAddNr'},
+  change       = {hl = 'GitGutterChange', text = '▎', numhl='GitSignsChangeNr'},
+  delete       = {hl = 'GitGutterDelete', text = '▎', numhl='GitSignsDeleteNr'},
+  topdelete    = {hl = 'GitGutterDelete', text = '▎', numhl='GitSignsDeleteNr'},
+  changedelete = {hl = 'GitGutterChange', text = '▎', numhl='GitSignsChangeNr'},
 }
 }
