c6d5755c5e13b173acef2fa9ce9fd3ae8e53a9d8
Cesar Leonardo <cesarleonardo537@gmail.com>
2021-06-07 16:07:56 -0600

    feat: changes

--- lua/flutter-tools/ui.lua
+++ lua/flutter-tools/ui.lua
@@ -287,6 +287,12 @@ function M.open_split(opts, on_open)
   end
   vim.bo[buf].swapfile = false
   vim.bo[buf].buftype = "nofile"
+  vim.wo[win].number = false
+  vim.wo[win].relativenumber = false
+  vim.wo[win].wrap = false
+  vim.bo[buf].buflisted = false
+  vim.bo[buf].bufhidden = "wipe"
+  vim.wo[win].winfixwidth = true
   if on_open then
     on_open(buf, win)
   end
