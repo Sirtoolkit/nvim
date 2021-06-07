7f34f587bd35eb6002ab3b86a11337b272ec2872
Cesar Leonardo <cesarleonardo537@gmail.com>
2021-06-07 15:49:29 -0600

    feat: changes

--- lua/flutter-tools-nvim.lua
+++ lua/flutter-tools-nvim.lua
@@ -7,11 +7,6 @@ require("flutter-tools").setup {
   widget_guides = {
     enabled = true,
   },
-  closing_tags = {
-    highlight = "ErrorMsg", -- highlight for the closing tag
-    prefix = ">", -- character to use for close tag e.g. > Widget
-    enabled = true, -- set to false to disable
-  },
   dev_tools = {
     autostart = false, -- autostart devtools server if not detected
   },
